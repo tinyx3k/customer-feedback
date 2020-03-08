1c93ce5bac883298f4db64c5c4a0465b356a0b99
rkg0612 <ralphkleinguevarra@gmail.com>
2020-03-08 21:50:32 +0800

    push

--- resources/views/modules/item/menu.blade.php
+++ resources/views/modules/item/menu.blade.php
@@ -4,7 +4,6 @@
 </style>
 @endsection
 @section('content')
-<h3 class="content-heading text-center font-w700 m-0 p-0">PELASE SELECT THE PRODUCT YOU ORDERED BELOW</h3>
 <div class="row justify-content-center">
     <div id="accordion" role="tablist" aria-multiselectable="true">
         @foreach($categories as $key => $category)
@@ -18,7 +17,7 @@
                         <div class="row">
                             @foreach($category->items as $item)
                             <div class="col-6 col-lg-4 col-md-4">
-                                <a href="{{ route('item.question', $item->id) }}" class="block block-rounded block-fx-shadow block-bordered text-center bg-primary-op">
+                                <a href="{{ auth()->user()->email == 'admin@admin.com' ? 'javascript:void(0);' : route('item.question', $item->id) }}" class="block block-rounded block-fx-shadow block-bordered text-center bg-primary-op">
                                     <div class="block-content block-content-full aspect-ratio-1-1 d-flex justify-content-center align-items-center">
                                         <div>
                                             <div class="font-size-h1 font-w300 text-white"><img src="{{ asset('/') .'img/item_images/'. $item->image }}" alt="{{ $item->name }}" class="img-avatar img-avatar96"></div>
