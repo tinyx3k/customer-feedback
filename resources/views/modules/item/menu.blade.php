0e5bbcf9f098b1d7bdcf5c95637e06aaf0fe87a8
rkg0612 <ralphkleinguevarra@gmail.com>
2020-03-10 08:14:54 +0800

    remove button

--- resources/views/modules/item/menu.blade.php
+++ resources/views/modules/item/menu.blade.php
@@ -53,7 +53,6 @@
                                             </div>
                                             <div class="block-content block-content-full text-right bg-light">
                                                 <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">Done</button>
                                             </div>
                                         </div>
                                     </div>
